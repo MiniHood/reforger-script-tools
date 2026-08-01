a1c917d3b66cb3a449ecfd3cc1155252f82cbea0
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2026-08-01 17:38:41 -0400

    fix: strip nested Wiki image links from previews

--- src/searchPrototype/searchUiPrototype.ts
+++ src/searchPrototype/searchUiPrototype.ts
@@ -393,7 +393,9 @@ const typeButtons = () => resultTypes.map(type => '<button class="' + (state.typ
 const inlineMarkdown = value => value
   .replace(/\\\\([*_])/g, '$1')
   .replace(/\`([^\`]+)\`/g, '<code>$1</code>')
-  .replace(/\\[([^\\]]+)\\]\\([^)]*\\)/g, '$1')
+  .replace(/!\\[([^\\]]*)\\]\\((?:[^()\\n]|\\([^()\\n]*\\))*\\)/g, '')
+  .replace(/\\[\\]\\((?:[^()\\n]|\\([^()\\n]*\\))*\\)/g, '')
+  .replace(/\\[([^\\]]+)\\]\\((?:[^()\\n]|\\([^()\\n]*\\))*\\)/g, '$1')
   .replace(/\\*\\*([^*]+)\\*\\*/g, '<strong>$1</strong>')
   .replace(/__([^_]+)__/g, '<strong>$1</strong>')
   .replace(/\\*([^*]+)\\*/g, '<em>$1</em>')
